9eed5c0990d266eb7667b69050d635536dce33f7
Alessandro <kalessandro14@gmail.com>
2019-03-27 11:47:20 -0400

    question model added + factories

--- database/migrations/2019_03_27_133347_create_questions_table.php
+++ database/migrations/2019_03_27_133347_create_questions_table.php
@@ -13,10 +13,23 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
+        //properties of a model
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedInteger('views')->default(0);//werid
+            $table->unsignedInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedInteger('best_answer_id')->nullable();//weird fk?
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            //FK
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            //we are going to have one for answers
         });
+
     }
 
     /**
