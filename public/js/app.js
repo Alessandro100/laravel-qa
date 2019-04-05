1e445dd826295720a26661f7249fc9cf56f6ed01
Alessandro <kalessandro14@gmail.com>
2019-04-05 09:58:08 -0400

    edit question

--- public/js/app.js
+++ public/js/app.js
@@ -49180,8 +49180,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/lavarelTest/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/lavarelTest/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/alessandro/Desktop/dev/php/lavarelTest/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/alessandro/Desktop/dev/php/lavarelTest/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
